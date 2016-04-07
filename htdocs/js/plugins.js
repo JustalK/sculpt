a268aa516fb9601a2b9f635eef8634e51ec2e624
Kevin Justal <justal.kevin@gmail.com>
2016-04-07 13:46:47 +0700

    Changing the scroll function for the top bar

--- htdocs/js/plugins.js
+++ htdocs/js/plugins.js
@@ -1,26 +1,28 @@
 // Avoid `console` errors in browsers that lack a console.
 (function() {
-	
-	var imgOffset = [0,0,0,1,1];
-	
+	var oldPositionTopScroll=0;
 	var positionTopScroll=0;
 	$(window).scroll(function(event) {
 		// Get the value of the scroll position
 		positionTopScroll = $(window).scrollTop();
 		resizeTopBar();
-		$(".imgHolder").css("top",-10*$(window).scrollTop()+"px");
+		$("#firstSlide").css("top",-10*$(window).scrollTop()+"px");
 	})
 	
-	var hiddenTopBar = false;
+	var scrollUp = false;
 	function resizeTopBar() {
-		if(positionTopScroll>10 && !hiddenTopBar) {
-			hiddenTopBar=true;
-			$("#topbar").animate({"top":"-"+$("#firstTopBar").height()+"px"},200);
-		} else if(positionTopScroll<10 && hiddenTopBar) {
-			hiddenTopBar=false;
+		if(oldPositionTopScroll-5>positionTopScroll && scrollUp) {
+			scrollUp=!scrollUp;
+			$("#topbar").clearQueue();
+			$("#topbar").stop();
 			$("#topbar").animate({"top":"0px"},200);
+		} else if(oldPositionTopScroll+5<positionTopScroll && !scrollUp) {
+			scrollUp=!scrollUp;
+			$("#topbar").clearQueue();
+			$("#topbar").stop();
+			$("#topbar").animate({"top":"-"+$("#firstTopBar").height()+"px"},200);
 		}
-		
+		oldPositionTopScroll = positionTopScroll;
 	}
 	
 	function adaptContent() {
@@ -34,19 +36,9 @@
 		var calW = $(".imgHolder").width() / 2400;
 		var calH = $(".imgHolder").height() / 1600;
 		$(".imgContainer").css("transform","translate3d(0px, 0px, 0px) scale("+calW+","+calW+")");
-		imgOffset[0] = 0;
-		imgOffset[1] = 0;
-		imgOffset[2] = 0;
-		imgOffset[3] = calW;
-		imgOffset[4] = calW;
 		if($(".imgContainer")[0].getBoundingClientRect().height<winH) {
 			var offsetX = ((calH*2400) - winW)/2;
 			$(".imgContainer").css("transform","translate3d(-"+offsetX+"px, 0px, 0px) scale("+calH+","+calH+")");
-			imgOffset[0] = offsetX;
-			imgOffset[1] = 0;
-			imgOffset[2] = 0;
-			imgOffset[3] = calH;
-			imgOffset[4] = calH;
 		}
 		
 	}
