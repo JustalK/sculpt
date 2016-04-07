e7a7ecad509a6830261436ab226e3c21bd930d51
Kevin Justal <justal.kevin@gmail.com>
2016-04-07 16:20:05 +0700

    Solving the bugs with the offset of transform's properties

--- htdocs/js/plugins.js
+++ htdocs/js/plugins.js
@@ -9,6 +9,20 @@
 		secondSlide();
 	})
 	
+	function secondSlide() {
+		if(positionTopScroll>50) {
+			var cal = 2*(positionTopScroll - 50);
+			$("#secondSlideText1").css("top","calc(100% - "+cal+"px)");
+				if(cal/250<0.5) {
+					$("#backgroundSlideText1").css("opacity",cal/250);
+				}
+			if($("#secondSlideText1").css("top")<-($(window).height()*50/100)) {
+				$("#backgroundSlideText1").css("opacity",cal/250);
+			}
+			console.log(($(window).height()*50/100)+" "+$("#secondSlideText1").css("top"));
+		}
+	}
+	
 	/**
 	 * Specialement pour l'animation sur le petit menu
 	 */
@@ -19,42 +33,17 @@
 			if (!$("#topbar").is(':animated')) {
 				$("#topbar").clearQueue();
 				$("#topbar").stop();
-				$("#topbar").animate({"top":"-"+$("#firstTopBar").height()+"px"},200);
+				$("#topbar").animate({"top":"-"+$("#firstTopBar").height()+"px"},100);
 			}
 		} else {
 			if (!$("#topbar").is(':animated')) {
 				$("#topbar").clearQueue();
 				$("#topbar").stop();
-				$("#topbar").animate({"top":"0px"},200);
+				$("#topbar").animate({"top":"0px"},100);
 			}
 		}
 		lastScrollTop = st;
 	});
-		
-		
-	function secondSlide() {
-		if(positionTopScroll>100) {
-			console.log("yes");
-		}
-	}
-	
-	var scrollUp = false;
-	function resizeTopBar() {
-		/**
-		if(oldPositionTopScroll-5>positionTopScroll && scrollUp) {
-			scrollUp=!scrollUp;
-			$("#topbar").clearQueue();
-			$("#topbar").stop();
-			$("#topbar").animate({"top":"0px"},200);
-		} else if(oldPositionTopScroll+5<positionTopScroll && !scrollUp) {
-			scrollUp=!scrollUp;
-			$("#topbar").clearQueue();
-			$("#topbar").stop();
-			$("#topbar").animate({"top":"-"+$("#firstTopBar").height()+"px"},200);
-		}
-		oldPositionTopScroll = positionTopScroll;
-		**/
-	}
 	
 	function adaptContent() {
 		$(".imgHolder").each(function( index ) {
@@ -66,10 +55,11 @@
 		var winH = $(window).height();
 		var calW = $(".imgHolder").width() / 1440;
 		var calH = $(".imgHolder").height() / 960;
-		$(".imgContainer").css("transform","translate3d(0px, -100px, 0px) scale("+calW+","+calW+")");
-		if($(".imgContainer")[0].getBoundingClientRect().height<winH) {
+		$(".imgContainer").css("transform","translate3d(0px, 0px, 0px) scale("+calW+","+calW+")");
+		console.log($(".imgHolder")[0].getBoundingClientRect().height+" "+$(".imgContainer")[0].getBoundingClientRect().height);
+		if($(".imgContainer")[0].getBoundingClientRect().height<winH || $(".imgHolder")[0].getBoundingClientRect().height<$(".imgContainer")[0].getBoundingClientRect().height) {
 			var offsetX = ((calH*1440) - winW)/2;
-			$(".imgContainer").css("transform","translate3d(-"+offsetX+"px, -100px, 0px) scale("+calH+","+calH+")");
+			$(".imgContainer").css("transform","translate3d(-"+offsetX+"px, 0px, 0px) scale("+calH+","+calH+")");
 		}
 		
 	}
@@ -80,5 +70,4 @@
 	$( window ).on("resize load",function() {
 		adaptContent();
 	});
-	
 }());
