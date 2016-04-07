33c5fcac2ec357c5249a0d247063bb51dbf59cb2
Kevin Justal <justal.kevin@gmail.com>
2016-04-07 17:21:49 +0700

    Trying the zoom

--- htdocs/js/plugins.js
+++ htdocs/js/plugins.js
@@ -9,20 +9,30 @@
 		secondSlide();
 	})
 	
+	var resW = 0;
+	var resH = 0;
+	var offset = 0;
 	function secondSlide() {
 		if(positionTopScroll>50) {
 			var cal = 2*(positionTopScroll - 50);
+			var effectOpacity = 250;
 			$("#secondSlideText1").css("top","calc(100% - "+cal+"px)");
-				if(cal/250<0.5) {
-					$("#backgroundSlideText1").css("opacity",cal/250);
+				if(cal/effectOpacity<0.5) {
+					$("#backgroundSlideText1").css("opacity",cal/effectOpacity);
 				}
-			if($("#secondSlideText1").css("top")<-($(window).height()*50/100)) {
-				$("#backgroundSlideText1").css("opacity",cal/250);
+			if(parseInt($("#secondSlideText1").css("top"))<-($(window).height()*50/100)) {
+				$("#backgroundSlideText1").css("opacity",2-cal/(effectOpacity+($(window).height()*50/100)));
+				//$("#backgroundSlideText1").css(cal)
+				//$("#backgroundSlideText1").css("opacity",$("#secondSlideText1").css("top")-0.5-());
 			}
-			console.log(($(window).height()*50/100)+" "+$("#secondSlideText1").css("top"));
+		}
+		if(positionTopScroll>480) {
+			zoom+=0.01;
+			var calW = $(".imgHolder").width() / 1440;
+			$(".imgContainer").css("transform","translate3d("+offset+"px, 0px, 0px) scale("+(calW+zoom)+","+(calW+zoom)+")");
 		}
 	}
-	
+	var zoom=0;
 	/**
 	 * Specialement pour l'animation sur le petit menu
 	 */
@@ -56,9 +66,14 @@
 		var calW = $(".imgHolder").width() / 1440;
 		var calH = $(".imgHolder").height() / 960;
 		$(".imgContainer").css("transform","translate3d(0px, 0px, 0px) scale("+calW+","+calW+")");
-		console.log($(".imgHolder")[0].getBoundingClientRect().height+" "+$(".imgContainer")[0].getBoundingClientRect().height);
+		resW = calW;
+		resH = calW;
+		offset = 0;
 		if($(".imgContainer")[0].getBoundingClientRect().height<winH || $(".imgHolder")[0].getBoundingClientRect().height<$(".imgContainer")[0].getBoundingClientRect().height) {
 			var offsetX = ((calH*1440) - winW)/2;
+			resW = calH;
+			resH = calH;
+			offset = offsetX;
 			$(".imgContainer").css("transform","translate3d(-"+offsetX+"px, 0px, 0px) scale("+calH+","+calH+")");
 		}
 		
